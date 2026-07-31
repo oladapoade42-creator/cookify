7a4280108ceef618bd0fe95c2a5146942e6d5e31
oladapoade42-creator <oladapoade42@gmail.com>
2026-07-31 11:47:25 +0100

    Removed vercel package

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-// 1. Import the generic injection function
-import {inject} from '@vercel/analytics';
-
-// 2.Initialize it safety at the root level
-inject();
+import App from './App.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
