e343d19e02d18c799cc4d528f919537c27fe050c
oladapoade42-creator <oladapoade42@gmail.com>
2026-07-31 11:32:42 +0100

    fix: update vercel analytics setup for vite

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// 1. Import the generic injection function
+import {inject} from '@vercel/analytics';
+
+// 2.Initialize it safety at the root level
+inject();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
